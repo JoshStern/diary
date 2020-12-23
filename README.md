150ef12e31fd6dcef6686d7983b237cecbd9c5a7
Josh Stern <josh.stern@compass.com>
2020-12-23 14:20:47 -0500

    Feat: Add ability to track diaries remotely
    - Adds details to README
    - Adds remote initialization and tracking to diary operations

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # Diary
-A simple CLI-driven diary.
+A very simple CLI diary using [`git`](https://git-scm.com) for history tracking.
+
+## Setup
+### Env Variables
+Diary uses a few environment variables:
+* `EDITOR` - Your choice of editor (default editor is `vim`). For vscode please be sure your variable includes the wait command (`code --wait`).
+* `DIARY_REPO` - Path to the repo used for the diary.
+* `DIARY_DEBUG` - When set to any non-zero length value this enables all command output to stdout/stderr.
+
+## Usage
+### Initializing a local diary
+To create a new diary local diary run `diary init [diary_repo]`. This will initialize a `git` repo at that location. If no arguments are provided then diary will attempt to initialize at `DIARY_REPO`.
+
+Diary currently supports a single-branch workflow using the `main` branch.
+
+**Example:**
+```sh
+# Initialize diary using DIARY_REPO
+export DIARY_REPO=$HOME/my_diary
+diary init
+
+# Initialize a second diary by providing a path
+diary init ~/my_second_diary
+```
+
+### Initializing a remote diary
+To create a new diary that's tracked remotely you can run `diary init <diary_repo> <diary_remote>`. Both arguments are required for the remote to be set up. Be sure that the remote repository is empty when the initialization script is run.
+
+**Example:**
+```sh
+# An empty remote exists at git@github.com:YourUsername/my_diary.git
+diary init ~/my_diary git@github.com:YourUsername/my_diary.git
+```
+
+## Opening a diary
+To open today's entry run `diary [diary_repo]`. This will either initialize a new entry or open an existing one based on the date. Once editing is done all changes are immediately committed (and pushed if a remote is set). The repo argument is optional if `DIARY_REPO` is defined.
+
+## Using a remote diary
+If a remote diary already exists then you can download it using `git clone` and begin using it immediately. The local repo will attempt to sync changes with the cloned diary.
+
+**Example:**
+```sh
+# A remote diary with entries exists at git@github.com:YourUsername/my_diary.git
+cd $HOME
+git clone git@github.com:YourUsername/my_diary.git
+
+# Open today's entry for that diary
+diary my_diary
+```
+
+## Autocomplete
+The source is shipped with `zsh/_diary` which can be used to setup autocomplete for `zsh`. If you're using `oh-my-zsh` it's as simple copying the `_diary` script to: `.oh-my-zsh/custom/plugins/diary/_diary` and adding it to the `plugins=(...)` list in your `.zshrc`.
--- bin/diary.sh
+++ bin/diary.sh
@@ -1,13 +1,46 @@
 #!/usr/bin/env bash
 
-set -e;set -u;
+set -u
 
 RED='\e[31m'
+GREEN='\e[32m'
 NC='\e[0m'
+
+# Print standard error message and exit.
+error () {
+  if [ ! -z "${1-}" ]; then
+    printf "${RED}[ERROR]${NC} %s\n" "$1"
+  fi
+  print_help
+  exit 1
+}
+
+# Print standard info message.
+log_info () {
+  printf "${GREEN}[DIARY]${NC} %s\n" "$1"
+}
+
+# Run command silently unless debug flag is set.
+quiet () {
+  if [ ! -z ${DIARY_DEBUG:-} ]; then
+    "$@"
+  else
+    "$@" > /dev/null 2>&1
+  fi
+}
+
+# Print help message
+print_help () {
+  printf "Diary Options:\n"
+  printf "\tdiary [diary_repo]: Open today's entry. Uses DIARY_REPO if no repo is provided and EDITOR (defaults to vim) for text editor.\n"
+  printf "\tdiary init [diary_repo] [diary_remote]: Initialize diary repo. Uses DIARY_REPO if no repo is provided.\n"
+  printf "\t\tIf both repo and remote are provided then a new diary is set up and tracked using the remote.\n"
+  printf "\n"
+}
 
 # Open diary command.
 # ```sh
-# open_diary <path-to-diary>
+# open_diary [diary_repo]
 # ```
 open_diary () {
   local repo=$1
@@ -21,45 +54,66 @@ open_diary () {
   local date_str=$(date +'%m-%d-%Y')
   local entry_name="entry_$date_str"
   local entry_path="$repo/$entry_name.md"
+  if has_origin $repo; then
+    log_info "Syncing with remote"
+    quiet git -C $repo pull
+  fi
 
-  # Open file with configured editor
   $editor $entry_path
 
-  # Commit changes
-  git -C $repo add -A
-  git -C $repo commit -m "edit $entry_name"
+  quiet git -C $repo add -A
+  quiet git -C $repo commit -m "edit $entry_name"
+  if has_origin $repo; then
+    log_info "Pushing to remote"
+    quiet git -C $repo push origin main
+  fi
+}
+
+# Check if repo has origin for tracking
+# ```sh
+# if has_origin $repo_path; then
+#    # Do someting
+# fi
+# ```
+has_origin() {
+  local repo=$1
+  local repo_remotes=$(git -C $repo remote)
+  for rem in $repo_remotes; do
+    if [ $rem == "origin" ]; then
+      true
+      return
+    fi
+  done
+  false
 }
 
 # Initialize diary fn
 # Called with the fully-qualified repo to be initialized:
 # ```sh
-# init_diary <path-to-diary-dir>
+# init_diary [local-repo] [remote-repo]
 # ```
 init_diary () {
   local repo=$1
-  echo "$repo"
+  local remote_repo=${2:-}
   # Validate the repo doesn't exist
   if [ -d "$repo/.git" ]; then
     error "Diary at '$repo' already exists."
   fi
+  log_info "Initializing repo '$repo'"
   mkdir -p $repo
-  git -C $repo init
-}
+  quiet git -C $repo init
+  echo "# Personal Diary" >> $repo/README.md
+  quiet git -C $repo add -A
+  quiet git -C $repo commit -m "Diary init commit"
+  quiet git -C $repo branch -M main
+  quiet git -C $repo config pull.rebase true
 
-# Help message
-print_help () {
-  printf "Diary Options:\n"
-  printf "\tdiary [diary_repo]: Open today's entry. Uses DIARY_REPO if no repo is provided and EDITOR (defaults to vim) for text editor.\n"
-  printf "\tdiary init [diary_repo]: Initialize diary repo. Uses DIARY_REPO if no repo is provided.\n"
-  printf "\n"
-}
-
-error () {
-  if [ ! -z "${1-}" ]; then
-    printf "${RED}ERROR: %s${NC}\n" "$1"
+  # Setup remote
+  if [ ! -z ${remote_repo:-} ]; then
+    log_info "Setting up remote '$remote_repo'"
+    quiet git -C $repo remote add origin $remote_repo
+    quiet git -C $repo push -u origin main
   fi
-  print_help
-  exit 1
 }
 
 # Parse arguments
@@ -69,8 +123,9 @@ do
 key="$1"
 case $key in
     init)
-      if [ ! -z ${2-} ]; then
+      if [ ! -z ${2:-} ]; then
         init_diary_repo=$2
+        init_diary_remote=${3:-}
         shift
       elif [ ! -z ${DIARY_REPO:-} ]; then
         init_diary_repo=$DIARY_REPO
@@ -90,10 +145,9 @@ case $key in
 esac
 done
 
-
 # Process arguments and call corresponding functions
 if [ ! -z ${init_diary_repo:-} ]; then
-  init_diary $init_diary_repo
+  init_diary $init_diary_repo ${init_diary_remote:-}
 else
   if [ ! -z ${positional_args[0]:-} ]; then
     diary_repo=${positional_args[0]}
